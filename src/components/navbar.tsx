79fc23ad0ccb9b65f90bc70bacf8f64b0e1e079e
rubenyh <rubenyanezhernandez@gmail.com>
2025-05-09 17:35:51 -0700

    initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+
+
+export default function navbar() {
+    <>
+        <nav>
+            My<span className="">Portfolio</span>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </>
+
+}
